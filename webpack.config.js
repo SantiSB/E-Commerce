80c9e257f9af2c447e434e2bba4ec707f3af19ff
santiagoprueba <santiago@gmail.com>
2022-11-29 23:51:27 -0500

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: "https://SantiSB.github.io/E-Commerce/#"
+    publicPath: "/E-Commerce/"
   },
   resolve: {
     extensions: ['.js', '.jsx'],
