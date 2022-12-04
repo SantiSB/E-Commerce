fad401fa7ff8826deab1f8734c0baeb62230965b
santiagoprueba <santiago@gmail.com>
2022-12-04 16:34:16 -0500

    update config

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrouserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
 
 const App = () => (
-  <HashRouter>
+  <BrouserRouter>
     <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
@@ -14,7 +14,7 @@ const App = () => (
         <Route component={NotFound} />
       </Switch>
     </Layout>
-  </HashRouter>
+  </BrouserRouter>
 );
 
 export default App;
